8ef3fbd02d76203ea3a7d2e03e42e21b84406488
ceciliaoneil <ceciliaoneil.di@gmail.com>
2022-11-21 13:45:53 +0000

    trafic light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,47 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState} from "react";
+import '../../styles/index.css';
 
 //create your first component
 const Home = () => {
+	const [selected1, setSelected1] = useState("no-shadow");
+	const [selected2, setSelected2] = useState("no-shadow");
+	const [selected3, setSelected3] = useState("no-shadow");
+
+	// Si es apretadop el color rojo
+	function PrendeRojo() {
+		if (selected1 === "no-shadow") {
+			setSelected1("shadow")
+			setSelected2("no-shadow")
+			setSelected3("no-shadow")
+		};
+	};
+
+	// Si es apretado el amarillo
+	function PrendeAmarillo() {
+		if (selected2 === "no-shadow") {
+			setSelected1("no-shadow")
+			setSelected2("shadow")
+			setSelected3("no-shadow")
+		};
+		};
+
+	// Si es apretado el verde
+	function PrendeVerde() {
+		if (selected3 === "no-shadow") {
+			setSelected1("no-shadow")
+			setSelected2("no-shadow")
+			setSelected3("shadow")
+		};
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container w-25 bg-dark d-flex justify-content-center">
+			<div>
+				<div className={"m-3 caja rounded-circle bg-danger "+selected1} onClick={PrendeRojo}></div>
+				<div className={"m-3 caja rounded-circle bg-warning "+selected2} onClick={PrendeAmarillo}></div>
+				<div className={"m-3 caja rounded-circle bg-success "+selected3} onClick={PrendeVerde}></div>
+			</div>
 		</div>
+		
 	);
 };
 
